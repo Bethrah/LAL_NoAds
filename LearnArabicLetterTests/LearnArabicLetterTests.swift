539efab520a0db80a86bbb7d54dee471f5504331
بدور <bdour@macbook-alkhas-b-bdwr.local>
2017-08-23 22:48:19 +0300

    Initial Commit

---
+++ LearnArabicLetterTests/LearnArabicLetterTests.swift
@@ -0,0 +1,36 @@
+//
+//  LearnArabicLetterTests.swift
+//  LearnArabicLetterTests
+//
+//  Created by بدور on ٢٣‏/٨‏/٢٠١٧.
+//  Copyright © ٢٠١٧ com.bethrah. All rights reserved.
+//
+
+import XCTest
+@testable import LearnArabicLetter
+
+class LearnArabicLetterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
